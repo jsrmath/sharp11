3408ba3b63c77cf9c9938546c55736526725509e
Julian Rosenblum <jsrmath@gmail.com>
2015-07-28 21:36:17 -0400

    Redid TraversableScale so that it contains a Scale instance and added .clean(), .transpose, and .shift()

--- test/scale.test.js
+++ test/scale.test.js
@@ -43,6 +43,11 @@ describe('Scale', function () {
       assert.equal(s.clean().toString(), 'Db D E Gb G A B');
       assert.equal(s.toString(), 'Db Ebb Fb Gb Abb Bbb Cb');
     });
+
+    it('should clean the notes in a traversable scale', function () {
+      var s = scale.create('Db', 'Locrian').traverse('Ebb4');
+      assert.equal(s.clean().toString(), 'Db4 [D4] E4 Gb4 G4 A4 B4');
+    });
   });
 
   describe('#transpose', function () {
@@ -52,26 +57,53 @@ describe('Scale', function () {
       assert.equal(s.key.name, 'F');
       assert.equal(s.toString(), 'F G A B C# D#');
     });
+
+    it('should transpose a traversable scale', function () {
+      var s = scale.create('C', 'Whole Tone').traverse('E4').transpose('P4');
+      assert.equal(s.toString(), 'F4 G4 [A4] B4 C#5 D#5');
+    });
   });
 
   describe('#traverse', function () {
     it('should create a traversable scale', function () {
       var s = scale.create('D', 'Major').traverse('G4');
-      assert.equal(s.current.toString(), 'G4');
-      assert.equal(s.root.toString(), 'D4');
+      assert.equal(s.current().toString(), 'G4');
+      assert.equal(s.scale.root.toString(), 'D4');
 
+      s = scale.create('D7', 'Major').traverse('G4');
+      assert.equal(s.scale.root.toString(), 'D4');
+
+      s = scale.create('G', 'Major').traverse('D4');
+      assert.equal(s.scale.root.toString(), 'G3');
+
+      s = scale.create('C', 'Major').traverse('C4');
+      assert.equal(s.scale.root.toString(), 'C4');
+    });
+
+    it('should throw errors when appropriate', function () {
       assert.throws(function () {
         s = scale.create('D', 'Major').traverse('G');
       });
 
-      s = scale.create('D7', 'Major').traverse('G4');
-      assert.equal(s.root.toString(), 'D4');
+      assert.throws(function () {
+        s = scale.create('D', 'Major').traverse('C4');
+      });
+    });
+  });
 
-      s = scale.create('G', 'Major').traverse('D4');
-      assert.equal(s.root.toString(), 'G3');
+  describe('#shift', function () {
+    it('should shift a traversable scale by a given number of steps', function () {
+      var s = scale.create('D', 'Major').traverse('G4');
 
-      s = scale.create('C', 'Major').traverse('C4');
-      assert.equal(s.root.toString(), 'C4');
+      assert.equal(s.shift(2).toString(), 'D4 E4 F#4 G4 A4 [B4] C#5');
+      assert.equal(s.shift(9).toString(), 'D5 E5 F#5 G5 A5 [B5] C#6');
+
+      assert.equal(s.shift(-2).toString(), 'D4 [E4] F#4 G4 A4 B4 C#5');
+      assert.equal(s.shift(-9).toString(), 'D3 [E3] F#3 G3 A3 B3 C#4');
+
+      assert.equal(s.shift(3).toString(), 'D4 E4 F#4 G4 A4 B4 [C#5]');
+      assert.equal(s.shift(-3).toString(), '[D4] E4 F#4 G4 A4 B4 C#5');
+      assert.equal(s.shift(0).toString(), 'D4 E4 F#4 [G4] A4 B4 C#5');
     });
   });
 
