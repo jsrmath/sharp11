af085d7f1bba26dd32e6307cf96d8291da4b06e8
Julian Rosenblum <jsrmath@gmail.com>
2015-08-07 00:19:48 -0400

    Added useSixteenth improv setting and more improv tests, fixed typo in rhythmicVariety

--- test/improv.test.js
+++ test/improv.test.js
@@ -41,5 +41,74 @@ describe('Improvisor', function () {
         imp = improv.create().over('chart', charts.myFunnyValentine);
       }
     });
+
+    it('should obey the `rests` setting', function () {
+      var imp = improv.create({rests: 1}).over('chart', charts.myFunnyValentine);
+      var foundRest = false;
+
+      imp.forEach(function (obj) {
+        obj.notes.forEach(function (arr) {
+          arr.forEach(function (note) {
+            if (!note) foundRest = true;
+          });
+        });
+      });
+
+      assert(foundRest);
+
+      imp = improv.create({rests: 0}).over('chart', charts.myFunnyValentine);
+
+      imp.forEach(function (obj) {
+        obj.notes.forEach(function (arr) {
+          arr.forEach(function (note) {
+            assert(note);
+          });
+        });
+      });
+    });
+
+    it('should obey the `rhythmicVariety` setting', function () {
+      var imp = improv.create({rhythmicVariety: .5}).over('chart', charts.myFunnyValentine);
+      var found = [false, false, false];
+
+      imp.forEach(function (obj) {
+        obj.notes.forEach(function (arr) {
+          found[arr.length - 2] = true;
+        });
+      });
+
+      assert(found[0]);
+      assert(found[1]);
+      assert(found[2]);
+
+      imp = improv.create({rhythmicVariety: 0}).over('chart', charts.myFunnyValentine);
+
+      imp.forEach(function (obj) {
+        obj.notes.forEach(function (arr) {
+          assert.equal(arr.length, 2);
+        });
+      });
+    });
+
+    it('should obey the `useSixteenths` setting', function () {
+      var imp = improv.create({useSixteenths: true, rhythmicVariety: .5}).over('chart', charts.myFunnyValentine);
+      var found4;
+
+      imp.forEach(function (obj) {
+        obj.notes.forEach(function (arr) {
+          if (arr.length === 4) found4 = true;
+        });
+      });
+
+      assert(found4);
+
+      imp = improv.create({useSixteenths: false, rhythmicVariety: .5}).over('chart', charts.myFunnyValentine);
+
+      imp.forEach(function (obj) {
+        obj.notes.forEach(function (arr) {
+          assert.notEqual(arr.length, 4);
+        });
+      });
+    });
   });
 });
