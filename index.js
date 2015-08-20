9e7920240d93c6bc88410125d222274eb056b4b4
Julian Rosenblum <jsrmath@gmail.com>
2015-08-19 23:39:36 -0400

    Added midi and improv to index.js exports

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports.note = require('./lib/note');
 module.exports.chord = require('./lib/chord');
 module.exports.scale = require('./lib/scale');
+module.exports.improv = require('./lib/improv');
+module.exports.midi = require('./lib/midi');
