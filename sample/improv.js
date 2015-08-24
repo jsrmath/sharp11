9289d1f00b66b4b914856c0dd334bd8e94082fc4
Julian Rosenblum <jsrmath@gmail.com>
2015-08-24 12:52:34 -0400

    Made all numerical improv settings rangeable and gave rests and rhythmicVariety ranges by default

--- sample/improv.js
+++ sample/improv.js
@@ -4,9 +4,9 @@ var _ = require('underscore');
 
 var imp = improv.create().over('chart', charts.myFunnyValentine);
 var imp2 = improv.create().over('chart', charts.takeTheATrain);
-var imp3 = improv.create({rests: [0.75, 0.25], rhythmicVariety: [0.25, 0.75]}).over('chart', charts.goodbyePorkPieHat);
+var imp3 = improv.create().over('chart', charts.goodbyePorkPieHat);
 var imp4 = improv.create({rests: [0.5, 0], rhythmicVariety: [0, 0.75], useSixteenths: false, onlyEighthRests: true}).over('chart', charts.giantSteps);
-var imp5 = improv.create({dissonance: 0, rests: [0.75, 0], rhythmicVariety: [0, 0.75]}).over('chart', charts.myFunnyValentineFull);
+var imp5 = improv.create({dissonance: 0}).over('chart', charts.myFunnyValentineFull);
 
 _.each([imp, imp2, imp3, imp4, imp5], function (imp) {
   console.log(imp.toString() + '\n');
