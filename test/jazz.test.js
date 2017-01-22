0855dd1bf70888d243a85bae4686689c151ce016
Julian Rosenblum <jsrmath@gmail.com>
2017-01-22 16:37:56 -0500

    Added symbol creation from both chords and Mehegan

--- test/jazz.test.js
+++ test/jazz.test.js
@@ -3,15 +3,15 @@ var jazz = require('../lib/jazz');
 var assert = require('assert');
 
 describe('Jazz', function () {
-  describe('Symbol', function () {
+  describe('symbolFromChord', function () {
     it('should create a valid symbol', function () {
-      var symbol = new jazz.Symbol('C', 'F7');
+      var symbol = jazz.symbolFromChord('C', 'F7');
       assert.equal(symbol.quality, 'x');
       assert.equal(symbol.interval, 5);
       assert.equal(symbol.numeral, 'IV');
       assert.equal(symbol.toString(), 'IVx');
 
-      symbol = new jazz.Symbol('A', 'D7');
+      symbol = jazz.symbolFromChord('A', 'D7');
       assert.equal(symbol.quality, 'x');
       assert.equal(symbol.interval, 5);
       assert.equal(symbol.numeral, 'IV');
@@ -19,33 +19,68 @@ describe('Jazz', function () {
     });
 
     it('should handle various qualities', function () {
-      assert.equal(new jazz.Symbol('C', 'F').quality, 'M');
-      assert.equal(new jazz.Symbol('C', 'F6').quality, 'M');
-      assert.equal(new jazz.Symbol('C', 'Fmaj7').quality, 'M');
+      assert.equal(jazz.symbolFromChord('C', 'F').quality, 'M');
+      assert.equal(jazz.symbolFromChord('C', 'F6').quality, 'M');
+      assert.equal(jazz.symbolFromChord('C', 'Fmaj7').quality, 'M');
 
-      assert.equal(new jazz.Symbol('C', 'Fm').quality, 'm');
-      assert.equal(new jazz.Symbol('C', 'Fm7').quality, 'm');
-      assert.equal(new jazz.Symbol('C', 'Fm9').quality, 'm');
-      assert.equal(new jazz.Symbol('C', 'FmM7').quality, 'm');
+      assert.equal(jazz.symbolFromChord('C', 'Fm').quality, 'm');
+      assert.equal(jazz.symbolFromChord('C', 'Fm7').quality, 'm');
+      assert.equal(jazz.symbolFromChord('C', 'Fm9').quality, 'm');
+      assert.equal(jazz.symbolFromChord('C', 'FmM7').quality, 'm');
 
-      assert.equal(new jazz.Symbol('C', 'F7').quality, 'x');
-      assert.equal(new jazz.Symbol('C', 'F9').quality, 'x');
+      assert.equal(jazz.symbolFromChord('C', 'F7').quality, 'x');
+      assert.equal(jazz.symbolFromChord('C', 'F9').quality, 'x');
 
-      assert.equal(new jazz.Symbol('C', 'Fdim').quality, 'o');
-      assert.equal(new jazz.Symbol('C', 'Fdim7').quality, 'o');
+      assert.equal(jazz.symbolFromChord('C', 'Fdim').quality, 'o');
+      assert.equal(jazz.symbolFromChord('C', 'Fdim7').quality, 'o');
 
-      assert.equal(new jazz.Symbol('C', 'Fm7b5').quality, 'ø');
-      assert.equal(new jazz.Symbol('C', 'F1/2dim7').quality, 'ø');
+      assert.equal(jazz.symbolFromChord('C', 'Fm7b5').quality, 'ø');
+      assert.equal(jazz.symbolFromChord('C', 'F1/2dim7').quality, 'ø');
 
-      assert.equal(new jazz.Symbol('C', 'Fsus4').quality, null);
+      assert.throws(function () {
+        jazz.symbolFromChord('C', 'Fsus4');
+      });
     });
 
     it('should handle various intervals', function () {
-      assert.equal(new jazz.Symbol('C', 'Eb').numeral, 'bIII');
-      assert.equal(new jazz.Symbol('C', 'A#').numeral, '#VI');
-      assert.equal(new jazz.Symbol('C', 'F#').numeral, '#IV');
-      assert.equal(new jazz.Symbol('C', 'Gb').numeral, 'bV');
-      assert.equal(new jazz.Symbol('C#', 'Bb').numeral, 'VI');
+      assert.equal(jazz.symbolFromChord('C', 'Eb').numeral, 'bIII');
+      assert.equal(jazz.symbolFromChord('C', 'A#').numeral, '#VI');
+      assert.equal(jazz.symbolFromChord('C', 'F#').numeral, '#IV');
+      assert.equal(jazz.symbolFromChord('C', 'Gb').numeral, 'bV');
+      assert.equal(jazz.symbolFromChord('C#', 'Bb').numeral, 'VI');
+    });
+  });
+
+  describe('symbolFromMehegan', function () {
+    it('should create a valid symbol', function () {
+      var symbol = jazz.symbolFromMehegan('IM');
+      assert.equal(symbol.quality, 'M');
+      assert.equal(symbol.interval, 0);
+      assert.equal(symbol.numeral, 'I');
+      assert.equal(symbol.toString(), 'IM');
+
+      symbol = jazz.symbolFromMehegan('bIIIx');
+      assert.equal(symbol.quality, 'x');
+      assert.equal(symbol.interval, 3);
+      assert.equal(symbol.numeral, 'bIII');
+      assert.equal(symbol.toString(), 'bIIIx');
+
+      symbol = jazz.symbolFromMehegan('iim');
+      assert.equal(symbol.quality, 'm');
+      assert.equal(symbol.interval, 2);
+      assert.equal(symbol.numeral, 'II');
+      assert.equal(symbol.toString(), 'IIm');
+
+      assert.throws(function () {
+        jazz.symbolFromMehegan('jk');
+      });
+    });
+
+    it('should infer chord qualities', function () {
+      assert.equal(jazz.symbolFromMehegan('I').quality, 'M');
+      assert.equal(jazz.symbolFromMehegan('II').quality, 'm');
+      assert.equal(jazz.symbolFromMehegan('VII').quality, 'ø');
+      assert.equal(jazz.symbolFromMehegan('bIII').quality, 'm');
     });
   });
 });
