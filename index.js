80ec121d1cf8677d8d5b45f605787bc5879121d7
Julian Rosenblum <jsrmath@gmail.com>
2017-05-17 22:12:58 -0400

    Expose interval and duration modules

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ module.exports.chord = require('./lib/chord');
 module.exports.scale = require('./lib/scale');
 module.exports.improv = require('./lib/improv');
 module.exports.midi = require('./lib/midi');
+module.exports.duration = require('./lib/duration');
+module.exports.interval = require('./lib/interval');
