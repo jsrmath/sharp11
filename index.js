93a2a21d831554a657d74f84b5deb3abcedb5c74
Julian Rosenblum <jsrmath@gmail.com>
2017-10-14 13:13:18 -0400

    Expose chart and corpus

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ module.exports.improv = require('./lib/improv');
 module.exports.midi = require('./lib/midi');
 module.exports.duration = require('./lib/duration');
 module.exports.interval = require('./lib/interval');
+module.exports.chart = require('./lib/chart');
+module.exports.corpus = require('./lib/corpus');
