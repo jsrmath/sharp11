d3fcaf0d7f6b90b288488fd510f2f3605ec5475d
Julian Rosenblum <jsrmath@gmail.com>
2017-10-14 13:41:06 -0400

    Fix remaining v2 migration issues

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports.note = require('./lib/note');
 module.exports.chord = require('./lib/chord');
 module.exports.scale = require('./lib/scale');
-module.exports.improv = require('./lib/improv');
 module.exports.midi = require('./lib/midi');
 module.exports.duration = require('./lib/duration');
 module.exports.interval = require('./lib/interval');
 module.exports.chart = require('./lib/chart');
 module.exports.corpus = require('./lib/corpus');
+module.exports.mehegan = require('./lib/mehegan');
